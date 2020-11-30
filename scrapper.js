582220a48fc5dd03e6d128c08276373c0692c25f
Justinlawrence102 <justin.lawrence102@gmail.com>
2020-11-30 16:32:58 -0500

    Made the Scapper and parseTOSText functions to be asyncronous. Now when you call scrape.js, it returns the TOS translated object

--- scrapper.js
+++ scrapper.js
@@ -8,21 +8,31 @@ const naturalLanguageParser = require('./parseTOSText');
 var url = "https://twitter.com/en/tos";
 var results = [];
 
+scrape(url)
+    .then(function(result){
+        console.log(result) //this should be the TOS data
+    })
+    .catch(function(error){
+        console.log("ERROR")
+});
 
 function scrape(link){
-	var tos = [];
-	axios.get(link)
-		.then((response)=> {
-			if(response.status === 200) {
-				const html = response.data;
-				const $ = cheerio.load(html);
-				$('p').each(async function(i, elem) {
-					tos.push($(this).text());
-				})
-                naturalLanguageParser.getPhrases(tos)
-			}
-		}, (error) => console.log(err) );
-		return tos;
+    var tos = [];
+    return new Promise(function(resolve, reject){
+        axios.get(link)
+        .then((response)=> {
+            if(response.status === 200) {
+                const html = response.data;
+                const $ = cheerio.load(html);
+                $('p').each(async function(i, elem) {
+                    tos.push($(this).text());
+                })
+               // resolve(tos);
+                resolve(naturalLanguageParser.getPhrases(tos));
+            }
+        }, (error) => console.log(err) );
+        //return tos;
+    });
 }
 
 results = scrape(url);
