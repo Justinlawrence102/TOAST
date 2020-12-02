de304791cdb83778489163cc57094833d03c3afb
Justinlawrence102 <justin.lawrence102@gmail.com>
2020-12-02 18:07:44 -0500

    it sort of works?

--- TOSSentenceObject.js
+++ TOSSentenceObject.js
@@ -7,20 +7,6 @@ function TOSSentence(sentence, isHighlight, phraseID, phrase, UIType, imageName,
     this.imageName = imageName;
     this.sourceURL = sourceURL;
     this.confidence = confidence;
-    
-    this.getDetails = function() {
-        console.log("Initial Sentence: "+this.sentence)
-        if (this.isHighlight) {
-            console.log("TOAST translation("+this.confidence+"): "+ this.phrase);
-            if (this.imageName != "") {
-                console.log("Image: "+this.imageName);
-            }
-        }
-        else {
-            console.log("Not translated!");
-        }
-        console.log("\n");
-    };
   }
 
 module.exports = TOSSentence;
--- index.js
+++ index.js
@@ -13,6 +13,8 @@ var link = "";
 var tos = [];
 
 app.use(express.json());
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 app.listen(PORT, () => console.log(`Express server currently running on port ${PORT}`));
 
@@ -32,26 +34,39 @@ app.get('/',(req,res) => {
 
 app.get('/results.html',(req,res)=> {
     link = req.query.link;
+    //var toastObjectArray = [{ sentence: "1", isHighlight: false},{ sentence: "Who May Use the Services You may use the Services only if you agree to form a binding contract with Twitter and are not a person barred from receiving services under the laws of the applicable jurisdiction", "isHighlight": true, "phraseID": 10, "phrase": "You agree to not misuse the Service", "UIType": "Pull Quotes and Factoids", "imageName": "id_10_do_not_misuse.png", "sourceURL": 'www.com', "confidence": 9}];
+
    //tos = tool.test(link);
+//    res.render('results.html',{
+//          test: "testing"
+//        });
     tool.callScrapper(link)
         .then(function(result){
-            console.log(result)
+            //var personObject = result.toString()
+            //console.log(result)
+            res.render('results.html',{
+            //var toastObjectArray = [{ 'sentence': '1', 'isHighlight': false},{ 'sentence': ' Who May Use the Services You may use the Services only if you agree to form a binding contract with Twitter and are not a person barred from receiving services under the laws of the applicable jurisdiction', 'isHighlight': true, 'phraseID': 10, 'phrase': 'You agree to not misuse the Service', 'UIType': 'Pull Quotes and Factoids', 'imageName': 'id_10_do_not_misuse.png', 'sourceURL': '', 'confidence': 9}];
+            test: JSON.stringify(result).replace(/\\/g, '\\\\').replace(/"/g, '\\\"')
+                });
+            fs.readFile(__dirname + "/results.html")
+                .then(contents => {
+                    res.setHeader("Content-Type", "text/html");
+                 //   res.sendData(result)
+                  //  res.json(result)
+                    res.writeHead(200);
+
+                    res.end(contents);
+                })
+                .catch(err => {
+                    res.writeHead(500);
+                    res.end(err);
+                    return;
+                });
         })
         .catch(function(error){
-            console.log("ERROR")
+            console.log(error)
     });
     //console.log(tos);
-    fs.readFile(__dirname + "/results.html")
-        .then(contents => {
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(200);
-            res.end(contents);
-        })
-        .catch(err => {
-            res.writeHead(500);
-            res.end(err);
-            return;
-        });
 });
 
 app.get('/index.html',(req,res) => {
