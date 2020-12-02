13e7c4907d68b0cec8bcbcdef6b698ab87c4de28
chase-root <croot@ufl.edu>
2020-12-02 18:38:05 -0500

    working? maybe

--- index.js
+++ index.js
@@ -34,33 +34,10 @@ app.get('/',(req,res) => {
 
 app.get('/results.html',(req,res)=> {
     link = req.query.link;
-    //var toastObjectArray = [{ sentence: "1", isHighlight: false},{ sentence: "Who May Use the Services You may use the Services only if you agree to form a binding contract with Twitter and are not a person barred from receiving services under the laws of the applicable jurisdiction", "isHighlight": true, "phraseID": 10, "phrase": "You agree to not misuse the Service", "UIType": "Pull Quotes and Factoids", "imageName": "id_10_do_not_misuse.png", "sourceURL": 'www.com', "confidence": 9}];
-
-   //tos = tool.test(link);
-//    res.render('results.html',{
-//          test: "testing"
-//        });
     tool.callScrapper(link)
         .then(function(result){
-            //var personObject = result.toString()
-            //console.log(result)
             res.render('results.html',{
-            //var toastObjectArray = [{ 'sentence': '1', 'isHighlight': false},{ 'sentence': ' Who May Use the Services You may use the Services only if you agree to form a binding contract with Twitter and are not a person barred from receiving services under the laws of the applicable jurisdiction', 'isHighlight': true, 'phraseID': 10, 'phrase': 'You agree to not misuse the Service', 'UIType': 'Pull Quotes and Factoids', 'imageName': 'id_10_do_not_misuse.png', 'sourceURL': '', 'confidence': 9}];
             test: JSON.stringify(result).replace(/\\/g, '\\\\').replace(/"/g, '\\\"')
-                });
-            fs.readFile(__dirname + "/results.html")
-                .then(contents => {
-                    res.setHeader("Content-Type", "text/html");
-                 //   res.sendData(result)
-                  //  res.json(result)
-                    res.writeHead(200);
-
-                    res.end(contents);
-                })
-                .catch(err => {
-                    res.writeHead(500);
-                    res.end(err);
-                    return;
                 });
         })
         .catch(function(error){
