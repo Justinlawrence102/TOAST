dc640a87bb96208b01dd7fbde5d8766c52f6c117
hannahwilliams44 <hannahwilliams44@ufl.edu>
2020-12-02 15:22:42 -0500

    attempt to fix contact page 5

--- mail_handler.php
+++ mail_handler.php
@@ -1,10 +1,14 @@
 <?php 
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+    set_error_handler("var_dump");
+    
     if(isset($_POST['submit'])){
         $to = "hannah.williams44.1997@gmail.com"; // this is your Email address
         $from = $_POST['email']; // this is the sender's Email address
         $name = $_POST['name'];
         $subject = "Form submission";
-        $message = $first_name . " from " . $_POST['email'] . " wrote the following:" . "\n\n" . $_POST['message'] ;
+        $message = $name . " from email address " . $_POST['email'] . " wrote the following:" . "\n\n" . $_POST['message'] ;
 
         $headers = "From:" . $from;
         $headers2 = "From:" . $to;
