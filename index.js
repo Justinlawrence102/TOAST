59fa2577df96f0136b0c47aa910c1365bf27e831
hannahwilliams44 <hannahwilliams44@ufl.edu>
2020-12-03 16:29:12 -0500

    attempt heroku fix 9.75

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const PORT = 3000;
 const path = require('path'); 
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/icons_vignettes")));
 //app.use(express.json());
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
